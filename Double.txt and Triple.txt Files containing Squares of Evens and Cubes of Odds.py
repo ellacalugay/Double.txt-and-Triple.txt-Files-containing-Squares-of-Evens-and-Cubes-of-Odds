60955684c3fd06a68e6578920cc6eb3a68757ed9
Ella <ellacalugay@gmail.com>
2023-04-23 23:05:06 +0800

    Pseudocode

--- Double.txt and Triple.txt Files containing Squares of Evens and Cubes of Odds.py
+++ Double.txt and Triple.txt Files containing Squares of Evens and Cubes of Odds.py
@@ -1,2 +1,11 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #4 | PROBLEM 4
 # Creating a program that will create to separate files containing the squares of all even and cubes of all odds.
+
+# Pseudocode
+# Open the file named integers.txt for reading, double.txt for appending, and tripple.txt for appending.
+    # Read each line in integers.txt.
+        # If the integer is even, square it. 
+            # Append the squared value of even integers to double.txt
+        # If the integer is odd, cube it.
+            # Append the cube value of odd integers to tripple.txt
+# End of the code.
