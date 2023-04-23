49205f3d5e908504dfca9dde50a6abd84aecccd0
Ella <ellacalugay@gmail.com>
2023-04-23 23:22:12 +0800

    Indention correction

--- Double.txt and Triple.txt Files containing Squares of Evens and Cubes of Odds.py
+++ Double.txt and Triple.txt Files containing Squares of Evens and Cubes of Odds.py
@@ -1,24 +1,24 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #4 | PROBLEM 4
 # Creating a program that will create to separate files containing the squares of all even and cubes of all odds.
 
-def computation():
+def computation(): # defining a function
 
 # Pseudocode
 # Open the file named integers.txt for reading, double.txt for appending, and tripple.txt for appending.
     with open ("integers.txt", "r") as integers, open("double.txt", "a") as squared, open("tripple.txt", "a") as cube:
-    # Read each line in integers.txt.
+        # Read each line in integers.txt.
         for line in integers:
             input_number = int(line)
-        # If the integer is even, square it. 
+            # If the integer is even, square it. 
             if input_number % 2 == 0:
                 square_even = input_number * input_number
-            # Append the squared value of even integers to double.txt
+                # Append the squared value of even integers to double.txt
                 squared.write (str(square_even) + "\n")
-        # If the integer is odd, cube it.
-        else:
-            cube_odd = input_number * input_number * input_number
-            # Append the cube value of odd integers to tripple.txt
-            cube.write (str(cube_odd) + "\n")
-            
-computation()
+            # If the integer is odd, cube it.
+            else:
+                cube_odd = input_number * input_number * input_number
+                # Append the cube value of odd integers to tripple.txt
+                cube.write (str(cube_odd) + "\n")
+
+computation() # calling a function
 # End of the code.
